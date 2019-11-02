0dc724809c45456c1b98269477c74b35b1eadf50
Bastien ALBANET <baalbane@e2r10p19.42.fr>
2019-11-02 20:04:48 +0100

    fact.c norme

--- srcs/fact.c
+++ srcs/fact.c
@@ -1,34 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fact.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbane <baalbane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/02 19:46:25 by baalbane          #+#    #+#             */
+/*   Updated: 2019/11/02 20:04:11 by baalbane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "main.h"
 
+t_fact		*fact_new(t_graph *graph, char name)
+{
+	t_fact	*new;
 
-t_fact *fact_new(t_graph *graph, char name) {
-    t_fact *new;
-
-    new = malloc(sizeof(t_fact));
-    new->node_type = NODE_TYPE_FACT;
-    new->name = name;
-    new->initial_value = 0;
-    new->set_value = 0;
-    add_new_node(graph, (nodes*)new);
-    return (new);
+	new = malloc(sizeof(t_fact));
+	new->node_type = NODE_TYPE_FACT;
+	new->name = name;
+	new->initial_value = 0;
+	new->set_value = 0;
+	add_new_node(graph, (nodes*)new);
+	return (new);
 }
 
-t_fact *get_fact(t_graph *graph, char name) {
-    int     i;
+t_fact		*get_fact(t_graph *graph, char name)
+{
+	int		i;
 
-    i = -1;
-    while (graph->facts[++i]) {
-        if (((t_fact*)(graph->facts[i]))->name == name) {
-            return (((t_fact*)(graph->facts[i])));
-        }
-    }
-    return (fact_new(graph, name));
+	i = -1;
+	while (graph->facts[++i])
+	{
+		if (((t_fact*)(graph->facts[i]))->name == name)
+			return (((t_fact*)(graph->facts[i])));
+	}
+	return (fact_new(graph, name));
 }
 
-ret_type init_fact_list(t_graph *graph) {
-    graph->fact_list_size = LIST_DEFAULT_SIZE;
-    graph->facts = malloc(sizeof(t_fact*) * graph->fact_list_size);
-    graph->facts[0] = NULL;
-    return (OK);
+ret_type	init_fact_list(t_graph *graph)
+{
+	graph->fact_list_size = LIST_DEFAULT_SIZE;
+	graph->facts = malloc(sizeof(t_fact*) * graph->fact_list_size);
+	graph->facts[0] = NULL;
+	return (OK);
 }
